27254d1bd70adb76854ec1a7a008c20984060c25
Larissa Lopes de Oliveira <152396589+Larissa-FLO@users.noreply.github.com>
2024-01-29 21:45:59 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Digite um número");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite outro número");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Escolha uma operação: + - * /");
+string operacao = Console.ReadLine();
+
+switch (operacao)
+{
+    case "+":
+        Console.WriteLine(num1 + num2);
+        break;
+    case "-":
+        Console.WriteLine(num1 - num2);
+        break;
+    case "*":
+        Console.WriteLine(num1 * num2);
+        break;
+    case "/":
+        if (num2 != 0)
+            Console.WriteLine(num1 / num2);
+        break;
+    default:
+        Console.WriteLine("Não é possível realizar a operação");
+        break;
+}
